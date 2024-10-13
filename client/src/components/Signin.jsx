9c84e9f01af5766f2275df61135c21b78b199eef
Noor Tabish Qureshy <164805722+noortabishq@users.noreply.github.com>
2024-10-14 00:48:34 +0530

    Update Signin.jsx

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,37 +1,98 @@
-import React from 'react'
-import Button from './Button'
+import React, { useState } from "react";
+import styled from "styled-components";
+import TextInput from "./TextInput";
+import Button from "./Button";
+import { UserSignIn } from "../api";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../redux/reducers/userSlice";
 
-function Signin() {
+const Container = styled.div`
+  width: 100%;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 36px;
+`;
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.text_primary};
+`;
+const Span = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary + 90};
+`;
+
+const SignIn = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateInputs = () => {
+    if (!email || !password) {
+      alert("Please fill in all fields");
+      return false;
+    }
+    return true;
+  };
+
+  const handelSignIn = async () => {
+    setLoading(true);
+    setButtonDisabled(true);
+    if (validateInputs()) {
+      await UserSignIn({ email, password })
+        .then((res) => {
+          dispatch(loginSuccess(res.data));
+          alert("Login Success");
+          setLoading(false);
+          setButtonDisabled(false);
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          setLoading(false);
+          setButtonDisabled(false);
+        });
+    }
+  };
+
   return (
-    <div className='md:w-[100vw] '>
-        <div className='flex items-center justify-center flex-col'>
-        <div>
-            <h1>Welcome to Get Fit</h1>
-            <span>Please Login with your deatails</span>
-        </div>
+    <Container>
+      <div>
+        <Title>Welcome to Get Fit</Title>
+        <Span>Please login with your details here</Span>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          flexDirection: "column",
+        }}
+      >
+        <TextInput
+          label="Email Address"
+          placeholder="Enter your email address"
+          value={email}
+          handelChange={(e) => setEmail(e.target.value)}
+        />
+        <TextInput
+          label="Password"
+          placeholder="Enter your password"
+          password
+          value={password}
+          handelChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          text="SignIn"
+          onClick={handelSignIn}
+          isLoading={loading}
+          isDisabled={buttonDisabled}
+        />
+      </div>
+    </Container>
+  );
+};
 
-        <form action="">
-            <div>
-                <label htmlFor="">Email
-                <input type="text" />
-                </label>
-                
-            </div>
-            <div>
-                <label htmlFor="">Password
-                <input type="password" />
-                </label>
-                
-            </div>
-            <span>do not have an account? SignUp</span>
-        <Button/>
-        </form>
-
-        </div>
-    </div>
-    
-
-  )
-}
-
-export default Signin
+export default SignIn;
