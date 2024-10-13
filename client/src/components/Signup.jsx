0dd3dc164077382e4043b36fa6446dd71ede8f52
Noor Tabish Qureshy <164805722+noortabishq@users.noreply.github.com>
2024-10-14 00:49:02 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,32 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import TextInput from "./TextInput";
+import Button from "./Button";
+import { UserSignUp } from "../api";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../redux/reducers/userSlice";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 36px;
+`;
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.text_primary};
+`;
+const Span = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary + 90};
+`;
 
 const SignUp = () => {
-  return (
-    <div>
-      <h1>Create New Account 👋</h1>
-      <p>Please enter details to create a new account</p>
-      <div>
-        <label>
-          Full name:
-          <input type="text" placeholder="Enter your full name" />
-        </label>
-        <label>
-          Email Address:
-          <input type="email" placeholder="Enter your email address" />
-        </label>
-        <label>
-          Password:
-          <input type="password" placeholder="Enter your password" />
-        </label>
-        <label>
-          Re-type Password:
-          <input type="password" placeholder="Enter your password" />
-        </label>
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        <span>already have an account? signin</span>
-        <button>Sign In</button>
+  const validateInputs = () => {
+    if (!name || !email || !password) {
+      alert("Please fill in all fields");
+      return false;
+    }
+    return true;
+  };
+
+  const handelSignUp = async () => {
+    setLoading(true);
+    setButtonDisabled(true);
+    if (validateInputs()) {
+      await UserSignUp({ name, email, password })
+        .then((res) => {
+          dispatch(loginSuccess(res.data));
+          alert("Account Created Success");
+          setLoading(false);
+          setButtonDisabled(false);
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          setLoading(false);
+          setButtonDisabled(false);
+        });
+    }
+  };
+  return (
+    <Container>
+      <div>
+        <Title>Create New Account</Title>
+        <Span>Please enter details to create a new account</Span>
       </div>
-    </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          flexDirection: "column",
+        }}
+      >
+        <TextInput
+          label="Full name"
+          placeholder="Enter your full name"
+          value={name}
+          handelChange={(e) => setName(e.target.value)}
+        />
+        <TextInput
+          label="Email Address"
+          placeholder="Enter your email address"
+          value={email}
+          handelChange={(e) => setEmail(e.target.value)}
+        />
+        <TextInput
+          label="Password"
+          placeholder="Enter your password"
+          password
+          value={password}
+          handelChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          text="SignUp"
+          onClick={handelSignUp}
+          isLoading={loading}
+          isDisabled={buttonDisabled}
+        />
+      </div>
+    </Container>
   );
 };
 
